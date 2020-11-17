cd5a34a69519401f5ecd45f47100edae982aebc9
stqin1992 <qinsongting1992@163.com>
2020-11-17 15:51:11 +0800

    新增 hutool工具包

--- TestDemo/src/test/java/com/stqin/study/java/Test1.java
+++ TestDemo/src/test/java/com/stqin/study/java/Test1.java
@@ -1,5 +1,6 @@
 package com.stqin.study.java;
 
+import cn.hutool.crypto.SecureUtil;
 import org.junit.Test;
 
 public class Test1 {
@@ -7,5 +8,10 @@ public class Test1 {
     @Test
     public void test(){
         System.out.println("123");
+    }
+
+    @Test
+    public void getHuTool() {
+        System.out.printf(SecureUtil.md5("123"));
     }
 }
